a527d896e358786fba822a6b025f3284b926104b
couturecraigj <couturecraigj@gmail.com>
2018-09-06 14:42:17 -0400

    Cleaning up some of the data

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,23 +1,34 @@
 import React from 'react'
 
 class Footer extends React.Component {
-    render() {
-        return (
-            <div id="footer">
-                <div className="inner">
-                    <ul className="icons">
-                        <li><a href="#" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
-                        <li><a href="#" className="icon fa-github"><span className="label">Github</span></a></li>
-                        <li><a href="#" className="icon fa-dribbble"><span className="label">Dribbble</span></a></li>
-                        <li><a href="#" className="icon fa-envelope-o"><span className="label">Email</span></a></li>
-                    </ul>
-                    <ul className="copyright">
-                        <li>&copy; Gatsby Starter Strata</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
-                    </ul>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div id="footer">
+        <div className="inner">
+          <ul className="icons">
+            <li>
+              <a
+                href="mailto:wullysammoth@gmail.com"
+                className="icon fa-envelope-o"
+              >
+                <span className="label">Email</span>
+              </a>
+            </li>
+          </ul>
+          <ul className="copyright">
+            <li>&copy; Gatsby Starter Strata</li>
+            <li>
+              Maintained:{' '}
+              <a href="https://github.com/couturecraigj">Craig Couture</a>
+            </li>
+            <li>
+              Design: <a href="http://html5up.net">HTML5 UP</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Footer
